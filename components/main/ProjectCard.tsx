26bbac58b42af45d2b1c1b9b36cf9fa5f10791b0
loshan20011 <loshan20011@gmail.com>
2024-03-02 13:43:42 +0530

    key-error-solved

--- components/main/ProjectCard.tsx
+++ components/main/ProjectCard.tsx
@@ -13,42 +13,31 @@ interface Props {
 
 const ProjectCard = ({ src, title, description, techStack, setClick }: Props) => {
   return (
-      <button onClick={setClick}
-        className="text-left relative z-50 project-primary overflow-hidden rounded-lg shadow-lg border border-[#333333]">
-        <Image
-          src={src}
-          alt={title}
-          width={1000}
-          height={1000}
-          className="md:max-h-[240px] w-full"
-        />
+    <button onClick={setClick} className="text-left relative z-50 project-primary overflow-hidden rounded-lg shadow-lg border border-[#333333]">
+      <Image
+        src={src}
+        alt={title}
+        width={1000}
+        height={1000}
+        className="md:max-h-[240px] w-full"
+      />
 
-        <div className="relative p-4">
-          <h1 className="text-2xl font-semibold text-white">{title}</h1>
-          <p className="mt-2 text-gray-400">{description}</p>
-          <div className="flex flex-col md:flex-row gap-2 mt-4">
-            <span className="text-gray-400">Tech Stack:</span>
-            <div className="flex flex-row md:gap-2 gap-2">
-              {techStack.map((tech, index) =>
-                techStack.length - 1 !== index ? (
-                  <div>
-                    <span key={index} className="block md:hidden text-gray-400">
-                      {tech} 
-                    </span>
-                    <span key={index} className="hidden md:block text-gray-400">
-                      {tech} |
-                    </span>
-                  </div>
-                ) : (
-                  <span key={index} className="text-gray-400">
-                    {tech}
-                  </span>
-                )
-              )}
-            </div>
+      <div className="relative p-4">
+        <h1 className="text-2xl font-semibold text-white">{title}</h1>
+        <p className="mt-2 text-gray-400">{description}</p>
+        <div className="flex flex-col md:flex-row gap-2 mt-4">
+          <span className="text-gray-400">Tech Stack:</span>
+          <div className="flex flex-row md:gap-2 gap-2">
+            {techStack.map((tech, index) => (
+              <div key={index}> {/* Add key prop here */}
+                <span className="block md:hidden text-gray-400">{tech}</span>
+                <span className="hidden md:block text-gray-400">{tech} |</span>
+              </div>
+            ))}
           </div>
         </div>
-      </button>
+      </div>
+    </button>
   );
 };
 
